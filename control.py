806630f8a3177aaad19c008e098c452d2c857171
Sebastian Wolf <Sebastian.Wolf@pace-systems.de>
2024-05-05 10:30:33 +0200

    Refactor control.py and put most code into main function

--- control.py
+++ control.py
@@ -24,88 +24,99 @@ import logging
 import logging.handlers
 from configparser import RawConfigParser
 
-from average import Average
 from component import allThreadsAlive
+from uptime import uptime, uptime_as_string
+from messageboard import message_board
+from signalshandler import SignalsHandler
+from average import Average
 from dcf77_thread import DCF77
 from devices import Devices
 from display import Display
-from fan import Fan
 from htmlwriter import HtmlWriter
 from menu import Menu
-from messageboard import messageboard
+from fan import Fan
 from sensor import Sensor
-from signals_handler import signals_handler
 from status import Status
-from uptime import Uptime, UptimeAsString
 from wlan import RestartWLAN, CheckNetwork
 
-config = RawConfigParser()
-config.read('fancontrol.cfg')
-logfile = config.get('logging', 'logfile')
 
-logdir = os.path.dirname(os.path.abspath(logfile))
-if not os.path.isdir(logdir):
-    os.makedirs(logdir)
+def main() -> None:
+    config = RawConfigParser()  # TODO: Use tomllib
+    config.read('fancontrol.cfg')
+    logfile = config.get('logging', 'logfile')
 
-logger = logging.getLogger('fancontrol')
-logger.setLevel(logging.INFO)
-class ContextFilter(logging.Filter):
-    def filter(self, record):
-        record.uptime = UptimeAsString()
-        return True
-logger.addFilter(ContextFilter())
-# File handler: rotate logs daily, keep logs > 2 years
-fh = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight', backupCount=750)
-class UTCFormatter(logging.Formatter):
-    converter = time.gmtime
-fh.setFormatter(UTCFormatter('%(asctime)s,%(uptime)s,%(levelno)s,%(filename)s,%(message)s'))
-logger.addHandler(fh)
-# Console handler: for warnings and errors
-ch = logging.StreamHandler()
-ch.setLevel(logging.WARNING)
-ch.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(message)s'))
-logger.addHandler(ch)
+    logdir = os.path.dirname(os.path.abspath(logfile))
+    if not os.path.isdir(logdir):
+        os.makedirs(logdir)
 
-logger.info('Startup')
+    logger = logging.getLogger('fancontrol')
+    logger.setLevel(logging.INFO)
 
-signals_handler = signals_handler(messageboard)
+    class ContextFilter(logging.Filter):
+        def filter(self, record) -> bool:
+            record.uptime = uptime_as_string()
+            return True
 
-with Display(), \
-     Sensor(), \
-     Status(), \
-     HtmlWriter(), \
-     Fan(), \
-     Menu(), \
-     Devices(), \
-     DCF77(), \
-     Average(), \
-     RestartWLAN(), \
-     CheckNetwork():
-    time0 = Uptime()
-    while messageboard.query('ExitThread') is None:
-        exception = messageboard.query('Exception')
-        if exception is not None:
-            raise exception
-        messageboard.post('Time', (Uptime(), time.localtime()))
-        time1 = Uptime()
-        if not time1 >= time0:
-            logger.warning('Error in uptime: {} < {}.'.format(time1, time0))
-            time0 = time1
-        sleeptime = 1 - time1 + time0
-        if sleeptime <= 0:
-            logger.warning(u'Zero sleep time: {} < {}, Δ={:.1f}s.'.
-                           format(time0, time1, time1-time0))
-        while sleeptime > 0:
-            time.sleep(sleeptime)
-            time1 = Uptime()
-            sleeptime = 1 - time1 + time0
-        if sleeptime > -.1:
-            time0 += 1
-        else:
-            logger.warning('Sleep longer than expected: {} < {}, Δ={:.1f}s.'.
-                           format(time0, time1, time1-time0))
-            time0 = time1
-        if not allThreadsAlive():
-            messageboard.post('ExitThread', True)
+    logger.addFilter(ContextFilter())
+    # File handler: rotate logs daily, keep logs > 2 years
+    fh = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight', backupCount=750)
 
-logger.info('Shutdown')
+    class UTCFormatter(logging.Formatter):
+        converter = time.gmtime
+
+    fh.setFormatter(UTCFormatter('%(asctime)s,%(uptime)s,%(levelno)s,%(filename)s,%(message)s'))
+    logger.addHandler(fh)
+    # Console handler: for warnings and errors
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARNING)
+    ch.setFormatter(logging.Formatter('%(asctime)s: %(levelname)s: %(message)s'))
+    logger.addHandler(ch)
+
+    logger.info('Startup')
+
+    signals_handler = SignalsHandler(message_board)
+
+    with (
+        Display(),
+        Sensor(),
+        Status(),
+        HtmlWriter(),
+        Fan(),
+        Menu(),
+        Devices(),
+        DCF77(),
+        Average(),
+        RestartWLAN(),
+        CheckNetwork(),
+    ):
+        logger.info('Start')
+        time0: float = uptime()
+        while message_board.query('ExitThread') is None:
+            exception = message_board.query('Exception')
+            if exception is not None:
+                raise exception
+            message_board.post('Time', (uptime(), time.localtime()))
+            time1 = uptime()
+            if time1 < time0:
+                logger.warning(f'Error in uptime: {time1} < {time0}.')
+                time0 = time1
+            sleep_time = 1 - time1 + time0
+            if sleep_time <= 0:
+                logger.warning(f'Zero sleep time: {time0} < {time1}, Δ={time1 - time0:.1f}s.')
+            while sleep_time > 0:
+                time.sleep(sleep_time)
+                time1 = uptime()
+                sleep_time = 1 - time1 + time0
+            if sleep_time > -.1:
+                time0 += 1
+            else:
+                logger.warning(f'Sleep longer than expected: {time0} < {time1}, Δ={time1 - time0:.1f}s.')
+                time0 = time1
+            if not allThreadsAlive():
+                message_board.post('ExitThread', True)
+
+    logger.info('Shutdown')
+
+
+if __name__ == '__main__':
+    main()
