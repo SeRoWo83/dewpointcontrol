7ad39bb07803280ab7395c606827719463002a78
Sebastian Wolf <Sebastian.Wolf@pace-systems.de>
2024-05-05 15:29:23 +0200

    Refactor htmlwriter adding typehints and applying pep8

--- htmlwriter.py
+++ htmlwriter.py
@@ -25,10 +25,15 @@ import os
 import shutil
 import time
 
+from numpy import isnan, NaN
+
 from ip import get_ip_address, get_wan_ip
 from uptime import uptime
 from component import Component
+from sht75 import SensorData
 
+COLOR_FAN = 'color:#FF8C00'
+COLOR_RED = 'color:red'
 
 prog_start = uptime()
 
@@ -36,75 +41,80 @@ DEBUG = False
 
 config = RawConfigParser()
 config.read('fancontrol.cfg')
-pagefilename = config.get('webserver', 'page')
-pagetempfilename = config.get('webserver', 'temppage')
-datafilename = config.get('webserver', 'data')
-datatempfilename = config.get('webserver', 'tempdata')
+page_filename = config.get('webserver', 'page')
+page_filename_temp = config.get('webserver', 'temppage')
+data_filename = config.get('webserver', 'data')
+data_filename_temp = config.get('webserver', 'tempdata')
 
-indexsource = config.get('webserver', 'indexsource')
-indextarget = config.get('webserver', 'indextarget')
-shutil.copyfile(indexsource, indextarget)
+index_source = config.get('webserver', 'indexsource')
+index_target = config.get('webserver', 'indextarget')
+shutil.copyfile(index_source, index_target)
 
-class Bunch:
-    def __init__(self, **kwds):
-        self.__dict__.update(kwds)
 
-def CSSstyle(x):
-    c = Bunch()
-    c.rH = '' if x.rH==x.rH else 'color:red'
-    c.T = '' if x.T==x.T else 'color:red'
-    c.tau = '' if x.tau==x.tau else 'color:red'
+def css_style(x: SensorData) -> SensorData:
+    c = SensorData()
+    c.rH = '' if isnan(x.humidity) else COLOR_RED
+    c.T = '' if isnan(x.temperature) else COLOR_RED
+    c.tau = '' if isnan(x.tau) else COLOR_RED
     return c
 
-def prettyPrint(number):
-    return '{:2.1f}'.format(number).replace('-', u'−')
+
+def pretty_print(number: float) -> str:
+    return f'{number:2.1f}'.replace('-', u'−')
+
 
 class PageGenerator:
-    def __init__(self):
-        self.statustxt = 'Status: Not set.'
-        self.statusstyle = 'color:red'
+    def __init__(self) -> None:
+        self.status_txt: str = 'Status: Not set.'
+        self.status_style: str = COLOR_RED
         self.set_mode(None)
-        self.set_fanstate(None)
+        self.set_fan_state(None)
         self.last_sync = None
-        self.S1 = Bunch(T=float('nan'), tau=float('nan'), rH=float('nan'))
-        self.S2 = Bunch(T=float('nan'), tau=float('nan'), rH=float('nan'))
+        self.sense1: SensorData = SensorData(temperature=NaN, tau=NaN, humidity=NaN)
+        self.sense2: SensorData = SensorData(temperature=NaN, tau=NaN, humidity=NaN)
 
-    def set_measurements(self, S1, S2):
-        self.S1 = S1
-        self.S2 = S2
+        self.fan_state_txt: str = 'Fan state: unknown.'
+        self.fan_state_style: str = COLOR_RED
+        self.mode_txt: str = ''
 
-    def set_status(self, status):
-        self.statustxt = status[0]
-        self.statusstyle = status[1]
+    def set_measurements(self, sense1: SensorData, sense2: SensorData) -> None:
+        self.sense1 = sense1
+        self.sense2 = sense2
+
+    def set_status(self, status: tuple[str, str, ...]) -> None:
+        self.status_txt = status[0]
+        self.status_style = status[1]
         self.last_sync = status[2]
 
-    def set_fanstate(self, state):
-        if state == 'FanOn':
-            self.fanstatetxt = 'Fan: On.'
-            self.fanstatestyle = 'color:#FF8C00'
-        elif state == 'FanOff':
-            self.fanstatetxt = 'Fan: Off.'
-            self.fanstatestyle = ''
-        elif state == 'OpenWindow':
-            self.fanstatetxt = 'Window is being opened.'
-            self.fanstatestyle = 'color:#FF8C00'
-        elif state == 'CloseWindow':
-            self.fanstatetxt = 'Window is being closed.'
-            self.fanstatestyle = 'color:#FF8C00'
-        else:
-            self.fanstatetxt = 'Fan state: unknown.'
-            self.fanstatestyle = 'color:red'
+    def set_fan_state(self, state: str | None) -> None:
+        match state:
+            case 'FanOn':
+                self.fan_state_txt = 'Fan: On.'
+                self.fan_state_style = COLOR_FAN
+            case 'FanOff':
+                self.fan_state_txt = 'Fan: Off.'
+                self.fan_state_style = ''
+            case 'OpenWindow':
+                self.fan_state_txt = 'Window is being opened.'
+                self.fan_state_style = COLOR_FAN
+            case 'CloseWindow':
+                self.fan_state_txt = 'Window is being closed.'
+                self.fan_state_style = COLOR_FAN
+            case _:
+                self.fan_state_txt = 'Fan state: unknown.'
+                self.fan_state_style = COLOR_RED
 
-    def set_mode(self, mode):
-        if mode == 'manual':
-            self.modetxt = 'Mode: manual. '
-        else:
-            self.modetxt = ''
+    def set_mode(self, mode: str | None) -> None:
+        match mode:
+            case 'manual':
+                self.mode_txt = 'Mode: manual. '
+            case _:
+                self.mode_txt = ''
 
-    def write(self):
+    def write(self) -> None:
         localtime = time.localtime()
-        with codecs.open(pagetempfilename, 'w', encoding='utf8') as f:
-            f.write(u'''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+        with codecs.open(page_filename_temp, 'w', encoding='utf8') as f:
+            f.write(f'''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
@@ -133,7 +143,7 @@ td.r {{text-align:right;}}
 <table>
   <tr>
     <td colspan="3">
-      {date}<span style="float:right">{time}</span>
+      {time.strftime("%d.%m.%Y", localtime)}<span style="float:right">{time}</span>
     </td>
   </tr>
   <tr class="hr">
@@ -150,157 +160,117 @@ td.r {{text-align:right;}}
     <td>
       Relative humidity in %
     </td>
-    <td class="c" style="{S1Color.rH}">
-      {S1rH}
+    <td class="c" style="{css_style(self.sense1).humidity}">
+      {pretty_print(self.sense1.humidity)}
     </td>
-    <td class="c" style="{S2Color.rH}">
-      {S2rH}
+    <td class="c" style="{css_style(self.sense2).humidity}">
+      {pretty_print(self.sense2.humidity)}
     </td>
   </tr>
   <tr>
     <td>
       Temperature in °C
     </td>
-    <td class="c" style="{S1Color.T}">
-      {S1T}
+    <td class="c" style="{css_style(self.sense1).temperature}">
+      {pretty_print(self.sense1.temperature)}
     </td>
-    <td class="c" style="{S2Color.T}">
-      {S2T}
+    <td class="c" style="{css_style(self.sense2).temperature}">
+      {pretty_print(self.sense2.temperature)}
     </td>
   </tr>
   <tr class="bg">
     <td>
       Dew point in °C
     </td>
-    <td class="c" style="{S1Color.tau}">
-      {S1tau}
+    <td class="c" style="{css_style(self.sense1).tau}">
+      {pretty_print(self.sense1.tau)}
     </td>
-    <td class="c" style="{S2Color.tau}">
-      {S2tau}
+    <td class="c" style="{css_style(self.sense2).tau}">
+      {pretty_print(self.sense2.tau)}
     </td>
   </tr>
   <tr>
-    <td colspan="3" style="{fanstatestyle}">
-      {modetxt}{fanstatetxt}
+    <td colspan="3" style="{self.fan_state_style}">
+      {self.mode_txt}{self.fan_state_txt}
     </td>
   </tr>
   <tr class="bg hr">
-    <td colspan="3" style="{statusstyle}">
-      {statustxt}
+    <td colspan="3" style="{self.status_style}">
+      {self.status_txt}
     </td>
   </tr>
   <tr>
     <td colspan="3">
-      IP Ethernet:&nbsp;<span style="float:right">{IPeth0}</span>
+      IP Ethernet:&nbsp;<span style="float:right">{get_ip_address('eth0')}</span>
     </td>
   </tr>
   <tr class="bg">
     <td colspan="3">
-      IP WLAN:&nbsp;<span style="float:right">{IPwlan0}</span>
+      IP WLAN:&nbsp;<span style="float:right">{get_ip_address('wlan0')}</span>
     </td>
   </tr>
   <tr>
     <td colspan="3">
-      IP WAN:&nbsp;<span style="float:right">{IPwan}</span>
+      IP WAN:&nbsp;<span style="float:right">{get_wan_ip()}</span>
     </td>
   </tr>
   <tr class="bg">
     <td colspan="3">
-      OS uptime:&nbsp;<span style="float:right">{uptime}</span>
+      OS uptime:&nbsp;<span style="float:right">{str(datetime.timedelta(seconds=int(uptime())))}</span>
     </td>
   </tr>
   <tr>
     <td colspan="3">
-      Controller uptime:&nbsp;<span style="float:right">{progtime}</span>
+      Controller uptime:&nbsp;<span style="float:right">{str(datetime.timedelta(
+                seconds=int(uptime() - prog_start)))}</span>
     </td>
   </tr>
   <tr class="bg">
     <td colspan="3">
-      Last DCF77 signal:&nbsp;<span style="float:right">{lastsync}</span>
+      Last DCF77 signal:&nbsp;<span style="float:right">{self.last_sync if self.last_sync else 'None'}</span>
     </td>
   </tr>
 </table>
 </body>
 </html>
-'''.
-                    format(date=time.strftime("%d.%m.%Y", localtime),
-                           time=time.strftime("%H:%M:%S", localtime),
-                           S1rH=prettyPrint(self.S1.rH),
-                           S2rH=prettyPrint(self.S2.rH),
-                           S1T=prettyPrint(self.S1.T),
-                           S2T=prettyPrint(self.S2.T),
-                           S1tau=prettyPrint(self.S1.tau),
-                           S2tau=prettyPrint(self.S2.tau),
-                           S1Color=CSSstyle(self.S1), S2Color=CSSstyle(self.S2),
-                           statustxt = self.statustxt,
-                           statusstyle = self.statusstyle,
-                           modetxt = self.modetxt,
-                           fanstatetxt = self.fanstatetxt,
-                           fanstatestyle = self.fanstatestyle,
-                           IPeth0=get_ip_address('eth0'),
-                           IPwlan0=get_ip_address('wlan0'),
-                           IPwan=get_wan_ip(),
-                           uptime=str(datetime.timedelta(seconds=int(Uptime()))),
-                           progtime=str(datetime.timedelta(seconds=int(Uptime() - progstart))),
-                           lastsync=self.last_sync if self.last_sync else 'None')
-            )
-        os.rename(pagetempfilename, pagefilename)
+''')
+        os.rename(page_filename_temp, page_filename)
 
-    def writedata(self):
+    def write_data(self) -> None:
         localtime = time.localtime()
-        with codecs.open(datatempfilename, 'w', encoding='utf8') as f:
-            f.write(u'''\
-{date}
-{time}
-{S1Color.rH}
-{S1rH}
-{S2Color.rH}
-{S2rH}
-{S1Color.T}
-{S1T}
-{S2Color.T}
-{S2T}
-{S1Color.tau}
-{S1tau}
-{S2Color.tau}
-{S2tau}
-{fanstatestyle}
-{modetxt}{fanstatetxt}
-{statusstyle}
-{statustxt}
-{IPeth0}
-{IPwlan0}
-{IPwan}
-{uptime}
-{progtime}
-{lastsync}'''.
-                    format(date=time.strftime("%d.%m.%Y", localtime),
-                           time=time.strftime("%H:%M:%S", localtime),
-                           S1rH=prettyPrint(self.S1.rH),
-                           S2rH=prettyPrint(self.S2.rH),
-                           S1T=prettyPrint(self.S1.T),
-                           S2T=prettyPrint(self.S2.T),
-                           S1tau=prettyPrint(self.S1.tau),
-                           S2tau=prettyPrint(self.S2.tau),
-                           S1Color=CSSstyle(self.S1), S2Color=CSSstyle(self.S2),
-                           statustxt = self.statustxt,
-                           statusstyle = self.statusstyle,
-                           modetxt = self.modetxt,
-                           fanstatetxt = self.fanstatetxt,
-                           fanstatestyle = self.fanstatestyle,
-                           IPeth0=get_ip_address('eth0'),
-                           IPwlan0=get_ip_address('wlan0'),
-                           IPwan=get_wan_ip(),
-                           uptime=str(datetime.timedelta(seconds=int(Uptime()))),
-                           progtime=str(datetime.timedelta(seconds=int(Uptime() - progstart))),
-                           lastsync=self.last_sync if self.last_sync else 'None')
-            )
-        os.rename(datatempfilename, datafilename)
+        with codecs.open(data_filename_temp, 'w', encoding='utf8') as f:
+            f.write(f'''\
+{time.strftime("%d.%m.%Y", localtime)}
+{time.strftime("%H:%M:%S", localtime)}
+{css_style(self.sense1).humidity}
+{pretty_print(self.sense1.humidity)}
+{css_style(self.sense2).humidity}
+{pretty_print(self.sense2.humidity)}
+{css_style(self.sense1).temperature}
+{pretty_print(self.sense1.temperature)}
+{css_style(self.sense2).temperature}
+{pretty_print(self.sense2.temperature)}
+{css_style(self.sense1).tau}
+{pretty_print(self.sense1.tau)}
+{css_style(self.sense2).tau}
+{pretty_print(self.sense2.tau)}
+{self.fan_state_style}
+{self.mode_txt}{self.fan_state_txt}
+{self.status_style}
+{self.status_txt}
+{get_ip_address('eth0')}
+{get_ip_address('wlan0')}
+{get_wan_ip()}
+{str(datetime.timedelta(seconds=int(uptime())))}
+{str(datetime.timedelta(seconds=int(uptime() - prog_start)))}
+{self.last_sync if self.last_sync else 'None'}''')
+        os.rename(data_filename_temp, data_filename)
 
-    def writeEndPage(self):
+    @staticmethod
+    def write_end_page() -> None:
         localtime = time.localtime()
-        with open(pagetempfilename, 'w') as f:
-            f.write('''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+        with open(page_filename_temp, 'w') as f:
+            f.write(f'''<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
@@ -316,54 +286,48 @@ window.onload = timedRefresh(10000);
 body {{font-family: sans-serif;}}
 </style>
 <body>
-{date}, {time}: Program exited.
+{time.strftime("%d.%m.%Y", localtime)}, {time.strftime("%H:%M:%S", localtime)}: Program exited.
 </body>
 </html>
-'''.
-                    format(date=time.strftime("%d.%m.%Y", localtime),
-                           time=time.strftime("%H:%M:%S", localtime))
-            )
-        os.rename(pagetempfilename, pagefilename)
+''')
+        os.rename(page_filename_temp, page_filename)
+
 
 class HtmlWriter(Component):
-    def __init__(self):
-        Component.__init__(self, 'HTML writer')
+    def __init__(self) -> None:
+        super().__init__('HTML writer')
         self.pageGenerator = PageGenerator()
-        self.oldstatus = (None, None)
+        self.status_old = (None, None)
 
-    def __enter__(self):
+    def __enter__(self) -> object:
         with self.lock:
-            self.messageboard.subscribe('Measurement', self, HtmlWriter.onMeasurement)
-            self.messageboard.subscribe('HTMLStatus', self, HtmlWriter.onHTMLStatus)
-            self.messageboard.subscribe('FanState', self, HtmlWriter.onFanState)
-            self.messageboard.subscribe('Mode', self, HtmlWriter.onMode)
-        return Component.__enter__(self)
+            self.message_board.subscribe('Measurement', self, HtmlWriter.on_measurement)
+            self.message_board.subscribe('HTMLStatus', self, HtmlWriter.on_html_status)
+            self.message_board.subscribe('FanState', self, HtmlWriter.on_fan_state)
+            self.message_board.subscribe('Mode', self, HtmlWriter.on_mode)
+        return super().__enter__()
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        Component.__exit__(self, exc_type, exc_value, traceback)
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        super().__exit__(exc_type, exc_value, traceback)
         with self.lock:
-            self.pageGenerator.writeEndPage()
+            self.pageGenerator.write_end_page()
 
-    def onMeasurement(self, message):
+    def on_measurement(self, message: tuple[str, SensorData, SensorData]):
         with self.lock:
             self.pageGenerator.set_measurements(*message[1:])
-            #self.pageGenerator.write() # ???
-            self.pageGenerator.writedata()
+            self.pageGenerator.write_data()
 
-    def onHTMLStatus(self, message):
+    def on_html_status(self, message: tuple[str, str, ...]) -> None:
         with self.lock:
             self.pageGenerator.set_status(message)
-            #self.pageGenerator.write()
-            self.pageGenerator.writedata()
+            self.pageGenerator.write_data()
 
-    def onFanState(self, message):
+    def on_fan_state(self, message: str) -> None:
         with self.lock:
-            self.pageGenerator.set_fanstate(message)
-            #self.pageGenerator.write()
-            self.pageGenerator.writedata()
+            self.pageGenerator.set_fan_state(message)
+            self.pageGenerator.write_data()
 
-    def onMode(self, message):
+    def on_mode(self, message: str) -> None:
         with self.lock:
             self.pageGenerator.set_mode(message)
-            #self.pageGenerator.write()
-            self.pageGenerator.writedata()
+            self.pageGenerator.write_data()
